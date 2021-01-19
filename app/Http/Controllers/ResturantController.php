1ae94a3599e10ef8c642e7cbdc62475185c363c9
Eslam Elgamal <me@esl4m.com>
2021-01-19 23:44:26 +0300

    Init sorting algorithm .. Init addFav to add favourite resturants . Init RemoveFav to remove favourite resturants

--- app/Http/Controllers/ResturantController.php
+++ app/Http/Controllers/ResturantController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Cookie;
 
 class ResturantController extends Controller
 {
+    public $resturants = [];
     /**
      * Instantiate a new controller Resturant.
      *
@@ -29,7 +31,93 @@ class ResturantController extends Controller
     public function index()
     {
         if ($this->my_resturants) {
-            return view('resturants', ['all_resturants' => $this->my_resturants]);
+            // Sort the resturants //
+            $this->resturants = $this->sortingList($this->my_resturants, 'open', 'minCost');
+
+            return view('resturants', ['all_resturants' => $this->resturants]);
         }
     }
+
+    /**
+     * Add Favorite Resturant
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function addFav(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove Favorite Resturant
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function removeFav(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Sort resturants
+     * 
+     * @param  array $new_resturants
+     * @param  $status
+     * @return array
+     */
+    public function sortingList($new_resturants, $status="open", $sortBy="bestMatch")
+    {
+        $asc = [
+            "newest",
+            "distance",
+            "averageProductPrice",
+            "deliveryCosts",
+            "minCost"
+        ];
+        
+        // Setting default values
+        if ($status == '') {
+            $status = 'open';
+        }
+        if ($sortBy == ''){
+            $sortBy = 'bestMatch';
+        }
+
+        switch ($status) {
+            case 'open':
+                $order = array('open', 'order ahead', 'closed');
+                break;
+
+            case 'order-ahead':
+                $order = array('order ahead', 'open', 'closed');
+                break;
+
+            case 'closed':
+                $order = array('closed', 'order ahead', 'open');
+                break;
+
+            default:
+                $order = array('open', 'order ahead', 'closed');
+        }
+
+        // Sorting by values
+        usort($new_resturants, function ($a, $b) use ($order, $sortBy, $asc) {
+            if ($a['status'] == $b['status']) {
+                // check if SortBy value is in ascending array:
+                if (in_array($sortBy, $asc)){
+                    return ($a['sortingValues'][$sortBy] - $b['sortingValues'][$sortBy]);
+                }
+                // else return descending 
+                return ($b['sortingValues'][$sortBy] - $a['sortingValues'][$sortBy]);
+            }
+            
+            $pos_a = array_search($a['status'], $order);
+            $pos_b = array_search($b['status'], $order);
+            return $pos_a - $pos_b;
+        });
+        
+        return $new_resturants;
+   }
 }
